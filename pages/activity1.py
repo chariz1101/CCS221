58dbdcd7d7e41bdfc6628987433a70dbcdf2ddd4
chariz1101 <charizdianne.falco@wvsu.edu.ph>
2023-03-01 12:08:56 +0800

    Plot Act1

--- pages/activity1.py
+++ pages/activity1.py
@@ -4,28 +4,42 @@ import matplotlib.pyplot as plt
 
 st.title("This is Activity 1")
 
-xstart = st.slider(
-    'Select a range of values',
+x1 = st.slider(
+    'X1',
     0.0, 100.0)
-st.write('Xstart: ', xstart)
+st.write('X1: ', x1)
 
+y1 = st.slider(
+    'Y1',
+    0.0, 100.0)
+st.write('Y1: ', y1)
 
-# def DDALine(x1, y1, x2, y2, color):
-#     dx = y2 - x1
-#     dy = y2 - y1
+x2 = st.slider(
+    'X2',
+    0.0, 100.0)
+st.write('X2: ', x2)
+
+y2 = st.slider(
+    'Y2',
+    0.0, 100.0)
+st.write('Y2: ', y2)
+
+color = "b." 
+dx = y2 - x1
+dy = y2 - y1
+
+steps = abs(dx) if abs(dx) > abs(dy) else abs(dy)
+
+Xinc = float(dx / steps)
+Yinc = float(dy / steps)
+
+for i in range(0, int(steps +1)):
     
-#     steps = abs(dx) if abs(dx) > abs(dy) else abs(dy)
+    plt.plot(int(x1), int(y1), color)
+    x1 += Xinc
+    y1 += Yinc
     
-#     Xinc = float(dx / steps)
-#     Yinc = float(dy / steps)
-    
-#     for i in range(0, int(steps +1)):
-        
-#         plt.plot(int(x1), int(y1), color)
-#         x1 += Xinc
-#         y1 += Yinc
-        
-#     plt.show()
+plt.show()
     
 
 # def main():
