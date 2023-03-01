d990bf670acf245d41c434795939f2729b94b7c5
chariz1101 <charizdianne.falco@wvsu.edu.ph>
2023-03-01 12:12:07 +0800

    Plot Act1 again

--- pages/activity1.py
+++ pages/activity1.py
@@ -24,31 +24,29 @@ y2 = st.slider(
     0.0, 100.0)
 st.write('Y2: ', y2)
 
-color = "b." 
-dx = y2 - x1
-dy = y2 - y1
 
-steps = abs(dx) if abs(dx) > abs(dy) else abs(dy)
-
-Xinc = float(dx / steps)
-Yinc = float(dy / steps)
-
-for i in range(0, int(steps +1)):
+def DDALine(x1, y1, x2, y2, color):
+    dx = y2 - x1
+    dy = y2 - y1
     
-    plt.plot(int(x1), int(y1), color)
-    x1 += Xinc
-    y1 += Yinc
     
-plt.show()
+    steps = abs(dx) if abs(dx) > abs(dy) else abs(dy)
+    
+    Xinc = float(dx / steps)
+    Yinc = float(dy / steps)
+    
+    for i in range(0, int(steps +1)):
+        
+        plt.plot(int(x1), int(y1), color)
+        x1 += Xinc
+        y1 += Yinc
+        
+    plt.show()
     
 
-# def main():
-#     st.slider() = int(input("Enter X1: "))
-#     st.slider() = int(input("Enter Y1: "))
-#     xEnd = int(input("Enter X2: "))
-#     yEnd = int(input("Enter Y2: "))
-#     color = "b." 
-#     DDALine(x, y, xEnd, yEnd, color)
+def main():
 
-# if __name__ == '__main__':
-#     main()    
+    DDALine(x, y, xEnd, yEnd, color)
+
+if __name__ == '__main__':
+    main()    
