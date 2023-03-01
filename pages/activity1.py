6f68a6c5ca38e98abf3cb41c072b17c6941cd19a
chariz1101 <charizdianne.falco@wvsu.edu.ph>
2023-03-01 09:42:47 +0800

    Act 1 new

--- pages/activity1.py
+++ pages/activity1.py
@@ -4,33 +4,37 @@ import matplotlib.pyplot as plt
 
 st.title("This is Activity 1")
 
-def DDALine(x1, y1, x2, y2, color):
-    dx = y2 - x1
-    dy = y2 - y1
+xstart = st.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0))
+st.write('Xstart: ', xstart)
+
+
+# def DDALine(x1, y1, x2, y2, color):
+#     dx = y2 - x1
+#     dy = y2 - y1
     
+#     steps = abs(dx) if abs(dx) > abs(dy) else abs(dy)
     
+#     Xinc = float(dx / steps)
+#     Yinc = float(dy / steps)
     
-    steps = abs(dx) if abs(dx) > abs(dy) else abs(dy)
-    
-    Xinc = float(dx / steps)
-    Yinc = float(dy / steps)
-    
-    for i in range(0, int(steps +1)):
+#     for i in range(0, int(steps +1)):
         
-        plt.plot(int(x1), int(y1), color)
-        x1 += Xinc
-        y1 += Yinc
+#         plt.plot(int(x1), int(y1), color)
+#         x1 += Xinc
+#         y1 += Yinc
         
-    plt.show()
+#     plt.show()
     
 
-def main():
-    x = int(input("Enter X1: "))
-    y = int(input("Enter Y1: "))
-    xEnd = int(input("Enter X2: "))
-    yEnd = int(input("Enter Y2: "))
-    color = "b." 
-    DDALine(x, y, xEnd, yEnd, color)
+# def main():
+#     st.slider() = int(input("Enter X1: "))
+#     st.slider() = int(input("Enter Y1: "))
+#     xEnd = int(input("Enter X2: "))
+#     yEnd = int(input("Enter Y2: "))
+#     color = "b." 
+#     DDALine(x, y, xEnd, yEnd, color)
 
-if __name__ == '__main__':
-    main()    
+# if __name__ == '__main__':
+#     main()    
