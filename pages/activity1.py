c0394368c8d52f76f8f2640ab7dbbd6e61718e56
chariz1101 <charizdianne.falco@wvsu.edu.ph>
2023-03-06 07:59:23 +0800

    Example 5,6

--- pages/activity1.py
+++ pages/activity1.py
@@ -37,7 +37,7 @@ for i in range(0, int(steps +1)):
         
 
         fig = plt.figure()
-        plt.plot([int(x1), int(y1)])
+        plt.plot([5, 6])
         plt.show()
         x1 += Xinc
         y1 += Yinc
