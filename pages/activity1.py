8eb9e1b122af9651f15a5db7c062e68437bc74f6
chariz1101 <charizdianne.falco@wvsu.edu.ph>
2023-03-06 08:05:06 +0800

    Edited code xEnd

--- pages/activity1.py
+++ pages/activity1.py
@@ -3,48 +3,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 import altair as alt
 
-st.title("This is Activity 1")
 
-x1 = st.slider(
-    'X1',
-    0, 100)
-st.write('Value of X1: ', x1)
 
-y1 = st.slider(
-    'Y1',
-    0, 100)
-st.write('Value of Y1: ', y1)
+def DDALine(x1, y1, x2, y2, color):
+    dx = y2 - x1
+    dy = y2 - y1
 
-x2 = st.slider(
-    'X2',
-    0, 100)
-st.write('Value of X2: ', x2)
+    steps = abs(dx) if abs(dx) > abs(dy) else abs(dy)
 
-y2 = st.slider(
-    'Y2',
-    0, 100)
-st.write('Value of Y2: ', y2)
+    Xinc = float(dx/steps)
+    Yinc = float(dy/steps)
 
-dx = y2 - x1
-dy = y2 - y1
+    for i in range(0, int(steps +1)):
+            plt.plot([int(x1), int(y1)])
+            x1 += Xinc
+            y1 += Yinc
+            
+    plt.show() 
+    fig = plt.figure()      
+    st.pyplot(fig)
 
-steps = abs(dx) if abs(dx) > abs(dy) else abs(dy)
-
-Xinc = float(dx/steps)
-Yinc = float(dy/steps)
-
-for i in range(0, int(steps +1)):
         
 
-        fig = plt.figure()
-        plt.plot([5, 6])
-        plt.show()
-        x1 += Xinc
-        y1 += Yinc
-        
-        
-st.pyplot(fig)
 
-     
+def main(): 
+    st.title("This is Activity 1")
+
+    x = st.slider(
+        'X1',
+        0, 100)
+    st.write('Value of X1: ', x)
+
+    y = st.slider(
+        'Y1',
+        0, 100)
+    st.write('Value of Y1: ', y)
+
+    xEnd = st.slider(
+        'X2',
+        0, 100)
+    st.write('Value of X2: ', xEnd)
+
+    yEnd = st.slider(
+        'Y2',
+        0, 100)
+    st.write('Value of Y2: ', yEnd)
+    color = "b." 
+
+    DDALine(x, y, xEnd, yEnd, color)
 
 
+if __name__ == '__main__':
+    main()    
