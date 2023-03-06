59bdc8a6364d42f4499071cfbac9a0a1d1d48c3c
chariz1101 <charizdianne.falco@wvsu.edu.ph>
2023-03-06 10:41:05 +0800

    imshow to figure

--- pages/activity2.py
+++ pages/activity2.py
@@ -20,7 +20,7 @@ def fill(x, y, replace):
         for j in range(len(two_d_arr[i])):
             two_d_arr[x][y] = replace
             
-    img = plt.imshow(two_d_arr)
+    img = plt.figure(two_d_arr)
     img.set_clim([1,1000])
     plt.colorbar()
     plt.show()
