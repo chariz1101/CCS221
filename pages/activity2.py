c19b3c7208ad333916d8427a8e86377917328655
chariz1101 <charizdianne.falco@wvsu.edu.ph>
2023-03-07 18:01:13 +0800

    Comments

--- pages/activity2.py
+++ pages/activity2.py
@@ -13,48 +13,48 @@ fig = px.imshow([[1, 20, 30],
                  [30, 60, 1]])
 fig.show()
 
-two_d_arr = np.array([[800, 800, 800],
-                      [800, 800, 800],
-                      [800, 800, 800]])
+# two_d_arr = np.array([[800, 800, 800],
+#                       [800, 800, 800],
+#                       [800, 800, 800]])
     
-x = []
-y = []
-replace = []
+# x = []
+# y = []
+# replace = []
     
-def fill(x, y, replace):
+# def fill(x, y, replace):
     
-    for i in range(len(two_d_arr)):
-        for j in range(len(two_d_arr[i])):
-            two_d_arr[x][y] = replace
+#     for i in range(len(two_d_arr)):
+#         for j in range(len(two_d_arr[i])):
+#             two_d_arr[x][y] = replace
             
-    img = plt.figure(two_d_arr)
-    img.set_clim([1,1000])
-    plt.colorbar()
-    plt.show()
-    st.pyplot(img)
+#     img = plt.figure(two_d_arr)
+#     img.set_clim([1,1000])
+#     plt.colorbar()
+#     plt.show()
+#     st.pyplot(img)
     
         
     
-def main():
+# def main():
 
-    x = st.slider(
-        'X',
-        0, 2)
-    st.write('Value of X: ', x)
+#     x = st.slider(
+#         'X',
+#         0, 2)
+#     st.write('Value of X: ', x)
 
-    y = st.slider(
-        'Y',
-        0, 2)
-    st.write('Value of Y: ', y)
+#     y = st.slider(
+#         'Y',
+#         0, 2)
+#     st.write('Value of Y: ', y)
 
-    replace = st.slider(
-        'Color',
-        1, 1000)
-    st.write('Color: ', replace)
+#     replace = st.slider(
+#         'Color',
+#         1, 1000)
+#     st.write('Color: ', replace)
 
-    fill(x, y, replace)
+#     fill(x, y, replace)
 
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
