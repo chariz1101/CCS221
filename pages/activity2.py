a274ed0b06c8125da0518ad1172fbf18eb298c32
chariz1101 <charizdianne.falco@wvsu.edu.ph>
2023-03-07 18:00:29 +0800

    Added code

--- pages/activity2.py
+++ pages/activity2.py
@@ -2,9 +2,16 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+
 st.title("This is Activity 2")
 
+import plotly.express as px
 
+fig = px.imshow([[1, 20, 30],
+                 [20, 1, 60],
+                 [30, 60, 1]])
+fig.show()
 
 two_d_arr = np.array([[800, 800, 800],
                       [800, 800, 800],
