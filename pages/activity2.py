46feea77dfe2c5545c2f25ef3e972a83a8e0f557
chariz1101 <charizdianne.falco@wvsu.edu.ph>
2023-03-08 16:38:11 +0800

    Try code from net

--- pages/activity2.py
+++ pages/activity2.py
@@ -2,53 +2,81 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
 st.title("This is Activity 2")
 
 
-two_d_arr = np.array([[800, 800, 800],
-                      [800, 800, 800],
-                      [800, 800, 800]])
+# two_d_arr = np.array([[800, 800, 800],
+#                       [800, 800, 800],
+#                       [800, 800, 800]])
     
-x = []
-y = []
-replace = []
+# x = []
+# y = []
+# replace = []
     
-def fill(x, y, replace):
+# def fill(x, y, replace):
     
-    for i in range(len(two_d_arr)):
-        for j in range(len(two_d_arr[i])):
-            two_d_arr[x][y] = replace
+#     for i in range(len(two_d_arr)):
+#         for j in range(len(two_d_arr[i])):
+#             two_d_arr[x][y] = replace
             
-    img = plt.figure(two_d_arr)
-    img.set_clim([1,1000])
-    plt.colorbar()
-    plt.show()
-    st.pyplot(img)
+#     img = plt.figure(two_d_arr)
+#     img.set_clim([1,1000])
+#     plt.colorbar()
+#     plt.show()
+#     st.pyplot(img)
     
         
     
-def main():
+# def main():
 
-    x = st.slider(
-        'X',
-        0, 2)
-    st.write('Value of X: ', x)
+#     x = st.slider(
+#         'X',
+#         0, 2)
+#     st.write('Value of X: ', x)
 
-    y = st.slider(
-        'Y',
-        0, 2)
-    st.write('Value of Y: ', y)
+#     y = st.slider(
+#         'Y',
+#         0, 2)
+#     st.write('Value of Y: ', y)
 
-    replace = st.slider(
-        'Color',
-        1, 1000)
-    st.write('Color: ', replace)
+#     replace = st.slider(
+#         'Color',
+#         1, 1000)
+#     st.write('Color: ', replace)
 
-    fill(x, y, replace)
+#     fill(x, y, replace)
 
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
+
+
+
+#importing required libraries
+
+import streamlit as st
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+
+
+#creating a sample array
+
+a = np.random.normal(1, 1, size=50)
+
+
+
+#specifying the figure to plot 
+
+fig, x = plt.subplots()
+
+x.hist(a, bins=10)
+
+
+
+#plotting the figure
+
+st.pyplot(fig)
