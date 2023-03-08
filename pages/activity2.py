7b6f22346628a873d51caa55e6664f18d7f22d17
chariz1101 <charizdianne.falco@wvsu.edu.ph>
2023-03-08 16:28:33 +0800

    Old code

--- pages/activity2.py
+++ pages/activity2.py
@@ -7,65 +7,48 @@ import matplotlib.pyplot as plt
 st.title("This is Activity 2")
 
 
-
-col1, col2 = st.columns(2)
-
-original = Image.open(image)
-col1.header("Original")
-col1.image(original, use_column_width=True)
-
-grayscale = original.convert('LA')
-col2.header("Grayscale")
-col2.image(grayscale, use_column_width=True)
-
-# Use the full page instead of a narrow central column
-st.set_page_config(layout="wide")
-
-# Space out the maps so the first one is 2x the size of the other three
-c1, c2, c3, c4 = st.columns((2, 1, 1, 1))
-
-# two_d_arr = np.array([[800, 800, 800],
-#                       [800, 800, 800],
-#                       [800, 800, 800]])
+two_d_arr = np.array([[800, 800, 800],
+                      [800, 800, 800],
+                      [800, 800, 800]])
     
-# x = []
-# y = []
-# replace = []
+x = []
+y = []
+replace = []
     
-# def fill(x, y, replace):
+def fill(x, y, replace):
     
-#     for i in range(len(two_d_arr)):
-#         for j in range(len(two_d_arr[i])):
-#             two_d_arr[x][y] = replace
+    for i in range(len(two_d_arr)):
+        for j in range(len(two_d_arr[i])):
+            two_d_arr[x][y] = replace
             
-#     img = plt.figure(two_d_arr)
-#     img.set_clim([1,1000])
-#     plt.colorbar()
-#     plt.show()
-#     st.pyplot(img)
+    img = plt.figure(two_d_arr)
+    img.set_clim([1,1000])
+    plt.colorbar()
+    plt.show()
+    st.pyplot(img)
     
         
     
-# def main():
+def main():
 
-#     x = st.slider(
-#         'X',
-#         0, 2)
-#     st.write('Value of X: ', x)
+    x = st.slider(
+        'X',
+        0, 2)
+    st.write('Value of X: ', x)
 
-#     y = st.slider(
-#         'Y',
-#         0, 2)
-#     st.write('Value of Y: ', y)
+    y = st.slider(
+        'Y',
+        0, 2)
+    st.write('Value of Y: ', y)
 
-#     replace = st.slider(
-#         'Color',
-#         1, 1000)
-#     st.write('Color: ', replace)
+    replace = st.slider(
+        'Color',
+        1, 1000)
+    st.write('Color: ', replace)
 
-#     fill(x, y, replace)
+    fill(x, y, replace)
 
 
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
