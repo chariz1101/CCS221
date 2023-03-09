b5ff2894f6d451bb32fb57d6de701fd56854f47e
chariz1101 <charizdianne.falco@wvsu.edu.ph>
2023-03-10 04:20:17 +0800

    Midpoint

--- pages/activity1.py
+++ pages/activity1.py
@@ -80,6 +80,49 @@ def bresenham(x1, y1, x2, y2, color):
     
     
     
+    
+    
+def midpoint(x1, y1, x2, y2, color): 
+   
+    x, y = x1, y1
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
+    x3 = (x1 + x2) / 2
+    y3 = (y1 + y2) / 2
+
+    slope = dy/float(dx)
+    
+    if slope > 1:
+        dx, dy = dy, dx
+        x, y = y, x
+        x1, y1 = y1, x1
+        x2 ,y2 = y2, x2
+
+    p = 2 * dy - dx
+    
+    xcords = [x]
+    ycords =[y]
+   
+    fig3=plt.figure()
+    for k in range(2,dx):
+        if p > 0:
+            y = y + 1 if y < y2 else y - 1
+            p = p + 2 * (dy - dx)
+        else :
+            p = p + 2 * dy
+        
+        x = x + 1 if x < x2 else x - 1
+        
+        xcords.append(x)
+        ycords.append(y)
+
+    st.write('Midpoint of the line is:', x3)
+    plt.plot(xcords,ycords,x3,y3,marker="o",markersize=5,markerfacecolor="red")
+    plt.show()
+    st.pyplot(fig3)
+    
+    
+    
 
 def main(): 
     st.title("This is Activity 1")
@@ -107,7 +150,7 @@ def main():
 
     DDALine(x, y, xEnd, yEnd, color)
     bresenham(x,y,xEnd, yEnd, color) # call for Bresenham's Line function
-    # midpoint(x,y,xEnd, yEnd, color)
+    midpoint(x,y,xEnd, yEnd, color)
 
 
 if __name__ == '__main__':
