cbd2442a31ab90f6097ae299108f80aa1839460b
Markndrei <119771465+Markndrei@users.noreply.github.com>
2023-03-11 03:10:22 +0800

    Update activity3.py

--- pages/activity3.py
+++ pages/activity3.py
@@ -21,12 +21,13 @@ def load_image (image_upload):
         img = Image.open (image_upload)
         return img
         
+   
 def image_ups ():
       uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
       for uploaded_file in uploaded_files:
-          bytes_data = uploaded_file.read()
-          st.write("filename:", uploaded_file.name)
-          st.write(bytes_data)
+         bytes_data = uploaded_file.read()
+         st.write("filename:", uploaded_file.name)
+         st.write(bytes_data)
         
         
         
