d83ae694d5075bc8131cbde7749d4921373063a8
Markndrei <119771465+Markndrei@users.noreply.github.com>
2023-03-11 19:22:20 +0800

    Update activity3.py

--- pages/activity3.py
+++ pages/activity3.py
@@ -7,16 +7,11 @@ png = str(".png")
 fig = plt.figure()
 path = str("pages/")
 
-Bx_old = []
-By_old = []
-Tx = []
-Ty = []
-
-def translation(Bx_old, By_old):
+def translation(i):
     
     #Translation
-    m_translation_ = np.float32([[1, 0, Bx_old],
-                                 [0, 1, By_old],
+    m_translation_ = np.float32([[1, 0, 20],
+                                 [0, 1, 10],
                                  [0, 0, 1]])
     
  
@@ -30,55 +25,99 @@ def translation(Bx_old, By_old):
     plt.show()
     st.pyplot(fig)
 
-
-def translation_new(Bx_new,By_new):
     
-    #Translation
-    m_translation_ = np.float32([[1, 0, Bx_new],
-                                 [0, 1, By_new],
-                                 [0, 0, 1]])
     
- 
+def rotation(i):
+    angle = np.radians(10)
+    m_rotation_ = np.float32([[np.cos(angle), -(np.sin(angle)), 0],
+                              [np.sin(angle), np.cos(angle), 0],
+                              [0, 0, 1]])
+    
+   
     image = cv2.imread(path + str(i) + png)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    cols, rows = (image.shape[:2])
+    cols, rows = image.shape[:2]
 
-    translated_image = cv2.warpPerspective(image, m_translation_, (cols, rows))
+    rotated_image = cv2.warpPerspective(image, m_rotation_, (int(cols), int(rows)))
     plt.axis('off')
-    plt.imshow(translated_image)
+    plt.imshow(rotated_image)
+    plt.show()
+    st.pyplot(fig)
+    
+    
+    
+def scaling(i):
+    m_scaling_ = np.float32([[1.5, 0, 0],
+                             [0, 1.8, 0],
+                             [0, 0, 1]])
+    
+   
+    image = cv2.imread(path + str(i) + png)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    cols, rows = image.shape[:2]
+
+    scaled_image = cv2.warpPerspective(image, m_scaling_, (cols*2, rows*2))
+    plt.axis('off')
+    plt.imshow(scaled_image)
+    plt.show()
+    st.pyplot(fig)
+    
+def shear(i):
+    
+    m_shearing_ = np.float32([[1, 0.5, 0],
+                               [0, 1, 0],
+                               [0, 0, 1]])
+    
+    
+    image = cv2.imread(path + str(i) + png)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    cols, rows = image.shape[:2]
+
+    sheared_image = cv2.warpPerspective(image, m_shearing_,(int(cols*1.5), int(rows*1.5)))
+    plt.axis('off')
+    plt.imshow(sheared_image)
     plt.show()
     st.pyplot(fig)
 
+def reflection(i):
+    image = cv2.imread(path + str(i) + png)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    cols, rows = image.shape[:2]
+    
+    m_reflection_ = np.float32([[1, 0, 0],
+                                [0, -1, rows],
+                                [0, 0, 1]])
+    
+  
+
+    reflected_image = cv2.warpPerspective(image, m_reflection_,(int(cols), int(rows)))
+    plt.axis('off')
+    plt.imshow(reflected_image)
+    plt.show()
+    st.pyplot(fig)
 
 def main () :
     st.title('This is Activity 3')
+    i = st.sidebar.slider('Choose Image [1, 2, 3]', 1, 3)
+    option = st.sidebar.selectbox('What shape would you like to rotate?', ('Translation', 'Rotation', 'Scaling', 'Shearing', 'Reflection'))
+    st.write('The image manipulation you chose is:', option)
     
-    x = st.sidebar.slider(
-        'Initial X',
-        0, 1000)
-    st.write('Value of X: ', Bx_old)
-
-    y = st.sidebar.slider(
-        'Initial Y',
-        0, 1000)
-    st.write('Value of Y: ', By_old)
-
-    x = st.sidebar.slider(
-        'Added X',
-        0, 1000)
-    st.write('Value of X: ', Tx)
-
-    y = st.sidebar.slider(
-        'Added Y',
-        0, 1000)
-    st.write('Value of Y: ', Ty)
-
-    Bx_new = Bx_old + Tx
-    By_new = By_old + Ty
-
-    translation(Bx_old, By_old)
-    translation_new(Bx_new, By_new)
-
+    if option == "Translation":
+        st.write("Translation")
+        translation(i)
+    if option == "Rotation":
+        st.write("Rotation")
+        rotation(i)
+    if option == "Scaling":
+        st.write("Scale")
+        scaling(i)
+    if option == "Shearing":
+        st.write("Shear")
+        shear(i)
+    if option == "Reflection":
+        st.write("Reflection")
+        reflection(i)
+    
     
 if __name__ == '__main__':
     main()
