c7bb77088f3f5d322c35219bc02610dc306f16c6
Markndrei <119771465+Markndrei@users.noreply.github.com>
2023-03-11 18:33:42 +0800

    Update activity3.py

--- pages/activity3.py
+++ pages/activity3.py
@@ -97,6 +97,7 @@ def reflection(i):
     st.pyplot(fig)
 
 def main () :
+    st.write('This is Activity 3')
     i = st.slider('Choose Image [1, 2, 3]', 1, 3)
     option = st.sidebar.selectbox('What shape would you like to rotate?', ('Translation', 'Rotation', 'Scaling', 'Shearing', 'Reflection'))
     st.write('The image manipulation you chose is:', option)
