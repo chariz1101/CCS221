28ffccf35ba5e199a49024271f7446c87fc2ee1d
chariz1101 <charizdianne.falco@wvsu.edu.ph>
2023-03-11 19:07:41 +0800

    Ako na sa Quiz Mark

--- pages/Quiz.py
+++ pages/Quiz.py
@@ -1,61 +1,84 @@
-
+import streamlit as st
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-image = cv2.imread("1.PNG")
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-cols, rows, dims = image.shape
+png = str(".png")
+fig = plt.figure()
+path = str("pages/")
 
 Bx_old = []
 By_old = []
 Tx = []
 Ty = []
 
-
-def translation_old(Bx_old, By_old):
-
-        
-        m_translation_ = np.float32([[1, 0, Bx_old],
-                                    [0, 1, By_old],
-                                    [0, 0, 1]
-                                    ])
+def translation(Bx_old, By_old):
     
-        translated_image = cv2.warpPerspective(image, m_translation_, (cols, rows))
+    #Translation
+    m_translation_ = np.float32([[1, 0, Bx_old],
+                                 [0, 1, By_old],
+                                 [0, 0, 1]])
     
-        plt.axis('off')
-        plt.imshow(translated_image)
-        plt.show()
-        
+ 
+    image = cv2.imread(path + str(i) + png)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    cols, rows = (image.shape[:2])
 
-        
-def translation_new(Bx_new, By_new):
-    
-        m_translation_ = np.float32([[1, 0, Bx_new],
-                                    [0, 1, By_new],
-                                    [0, 0, 1]
-                                    ])
-    
-        translated_image = cv2.warpPerspective(image, m_translation_, (cols, rows))
-    
-        plt.axis('off')
-        plt.imshow(translated_image)
-        plt.show()
+    translated_image = cv2.warpPerspective(image, m_translation_, (cols, rows))
+    plt.axis('off')
+    plt.imshow(translated_image)
+    plt.show()
+    st.pyplot(fig)
 
-        
-def main():
 
-        Bx_old = int(input("Enter Initial X: "))
-        By_old = int(input("Enter Initial Y: "))
-        Tx = int(input("Enter Translation X: "))
-        Ty = int(input("Enter Translation Y: "))
-        
-        Bx_new = Bx_old + Tx
-        By_new = By_old + Ty
-        
-        translation_old(Bx_old, By_old)
-        
-        translation_new(Bx_new, By_new)
+def translation_new(Bx_new,By_new):
+    
+    #Translation
+    m_translation_ = np.float32([[1, 0, Bx_new],
+                                 [0, 1, By_new],
+                                 [0, 0, 1]])
+    
+ 
+    image = cv2.imread(path + str(i) + png)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    cols, rows = (image.shape[:2])
+
+    translated_image = cv2.warpPerspective(image, m_translation_, (cols, rows))
+    plt.axis('off')
+    plt.imshow(translated_image)
+    plt.show()
+    st.pyplot(fig)
+
+
+def main () :
+    st.title('This is Activity 3')
+    
+    x = st.sidebar.slider(
+        'Initial X',
+        0, 1000)
+    st.write('Value of X: ', Bx_old)
+
+    y = st.sidebar.slider(
+        'Initial Y',
+        0, 1000)
+    st.write('Value of Y: ', By_old)
+
+    x = st.sidebar.slider(
+        'Added X',
+        0, 1000)
+    st.write('Value of X: ', Tx)
+
+    y = st.sidebar.slider(
+        'Added Y',
+        0, 1000)
+    st.write('Value of Y: ', Ty)
+
+    Bx_new = Bx_old + Tx
+    By_new = By_old + Ty
+
+    translation(Bx_old, By_old)
+    translation_new(Bx_new, By_new)
+
     
 if __name__ == '__main__':
-       main()
+    main()
