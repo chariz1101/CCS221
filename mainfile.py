a170238a2982e21ac0a142effcad6f483de248cd
chariz1101 <charizdianne.falco@wvsu.edu.ph>
2023-03-13 13:39:48 +0800

    Members

--- mainfile.py
+++ mainfile.py
@@ -2,4 +2,7 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.title("Hello")
+st.title("Group 3 - Fresnel")
+st.write("Deocampo, Cherilyn Marie")
+st.write("Encanto, Mark Andrei")
+st.write("Falco, Chariz Dianne")
