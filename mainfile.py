3ff4708bdcf2614ad5c53c4d7fbcd4171dc1339e
chariz1101 <charizdianne.falco@wvsu.edu.ph>
2023-03-13 13:41:12 +0800

    Section

--- mainfile.py
+++ mainfile.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.title("Group 3 - Fresnel")
+st.title("Group 3 - Fresnel (BSCS 1-A)")
 st.write("Deocampo, Cherilyn Marie")
 st.write("Encanto, Mark Andrei")
 st.write("Falco, Chariz Dianne")
